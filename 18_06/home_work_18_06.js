8fc557345754794b6355bc77b5997fcd6d64f5a2
andoreyrich <andoreyrich@gmail.com>
2020-06-18 22:00:06 +0200

    search dividers

--- 18_06/home_work_18_06.js
+++ 18_06/home_work_18_06.js
@@ -129,7 +129,7 @@ function divider() {
     for (let i = 1; i <= num; i++){
         if ( num % i === 0 ){ out.push(i) }
     }
-    document.querySelector('.out-7').innerHTML = `${out} &nbsp;&nbsp; - &nbsp;&nbsp; is dividers <b>${num}</b>`
+    document.querySelector('.out-7').innerHTML = `${out.join(', ')} &nbsp; - &nbsp; is dividers ${num}`
 }
 
 document.querySelector('.b-7').onclick = divider;
@@ -138,10 +138,56 @@ document.querySelector('.b-7').onclick = divider;
 // Задача
 // Даны 2 инпута и кнопка. В инпуты вводятся числа. По нажатию на кнопку выведите список общих делителей этих двух чисел.
 
+function dividers1() {
+    const num1 = document.querySelector('.i-81').value;
+    const num2 = document.querySelector('.i-82').value;
+    const out1 = [], out2 = [];
+    for (let i = 1; i <= num1; i++){ if ( num1 % i === 0 ){ out1.push(i) } }
+    for (let i = 1; i <= num2; i++){ if ( num2 % i === 0 ){ out2.push(i) } }
+
+    const out = out1.filter(el => out2.includes(el));
+
+    document.querySelector('.out-8').innerHTML = `${out.join(', ')} &nbsp; - &nbsp; common dividers for ${num1} and ${num2}`
+}
+
+document.querySelector('.b-8').onclick = dividers1;
+
 
 // Задача
 // Даны 2 инпута и кнопка. В инпуты вводятся числа. По нажатию на кнопку выведите наибольший общий делитель этих двух чисел.
 
+function dividers2() {
+    const num1 = document.querySelector('.i-91').value;
+    const num2 = document.querySelector('.i-92').value;
+    const out1 = [], out2 = [];
+    for (let i = 1; i <= num1; i++){ if ( num1 % i === 0 ){ out1.push(i) } }
+    for (let i = 1; i <= num2; i++){ if ( num2 % i === 0 ){ out2.push(i) } }
+
+    const out = out1.filter(el => out2.includes(el)).sort((a,b) => b - a);
+
+    document.querySelector('.out-9').innerHTML = `${out[0]} &nbsp; - &nbsp; the biggest divisor for  ${num1} and ${num2}`
+}
+
+document.querySelector('.b-9').onclick = dividers2;
+
 
 // Задача
 // Даны 2 инпута и кнопка. В инпуты вводятся числа. По нажатию на кнопку выведите наименьшее число, которое делится и на одно, и на второе из введенных чисел.
+
+function dividers3() {
+    const num1 = document.querySelector('.i-101').value;
+    const num2 = document.querySelector('.i-102').value;
+    const out = [];
+    for (let i = 1; i <= num1 * num2; i++){ if ( i % num1 === 0 && i % num2 === 0 ){ out.push(i) } }
+
+    document.querySelector('.out-10').innerHTML = `${out[0]} &nbsp; - &nbsp; the least divisible for  ${num1} and ${num2}`
+}
+
+document.querySelector('.b-10').onclick = dividers3;
+
+
+//
+
+
+
+
