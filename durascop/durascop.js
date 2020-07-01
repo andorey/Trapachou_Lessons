d34fcb5c70a6416866c2e56840bfafb00d4c4673
andoreyrich <andoreyrich@gmail.com>
2020-07-01 06:07:25 +0200

    create conditions

--- durascop/durascop.js
+++ durascop/durascop.js
@@ -7,202 +7,140 @@
 // Реализуйте показ гороскопа за текущий день.
 // А теперь реализуйте работу радиокнопок, позволяющих выбрать день, за который нужно показать гороскоп.
 
-const horoArr = {
+const horoDict = {
     "aries": {
         "symbol": "♈",
         "dateMin": "2000-03-21",
-        "dateMax": "2000-04-20"
-    },
-    "taurus": {
-        "symbol": "♉",
-        "dateMin": "2000-04-21",
-        "dateMax": "2000-05-21"
-    },
-    "gemini": {
-        "symbol": "♊",
-        "dateMin": "2000-05-22",
-        "dateMax": "2000-06-21"
-    },
-    "cancer": {
-        "symbol": "♋",
-        "dateMin": "2000-06-22",
-        "dateMax": "2000-07-22"
-    },
-    "leo": {
-        "symbol": "♌",
-        "dateMin": "2000-07-23",
-        "dateMax": "2000-08-22"
-    },
-    "virgo": {
-        "symbol": "♍",
-        "dateMin": "2000-08-23",
-        "dateMax": "2000-09-23"
-    },
-    "libra": {
-        "symbol": "♎",
-        "dateMin": "2000-09-24",
-        "dateMax": "2000-10-23"
-    },
-    "scorpio": {
-        "symbol": "♏",
-        "dateMin": "2000-10-24",
-        "dateMax": "2000-11-22"
-    },
-    "sagittarius": {
-        "symbol": "♐",
-        "dateMin": "2000-11-23",
-        "dateMax": "2000-12-21"
-    },
-    "capricorn": {
-        "symbol": "♑",
-        "dateMin": "2000-12-22",
-        "dateMax": "2000-01-20"
-    },
-    "aquarius": {
-        "symbol": "♒",
-        "dateMin": "2000-01-21",
-        "dateMax": "2000-02-19"
-    },
-    "pisces": {
-        "symbol": "♓",
-        "dateMin": "2000-02-20",
-        "dateMax": "2000-03-20"
-    }
-}
-
-const stouns = {
-    "aries": {
-        "name": "Aries",
+        "dateMax": "2000-04-20",
         "element": "fire",
         "stone": "bloodstone"
     },
     "taurus": {
-        "name": "Taurus",
+        "symbol": "♉",
+        "dateMin": "2000-04-21",
+        "dateMax": "2000-05-21",
         "element": "earth",
         "stone": "sapphire"
     },
     "gemini": {
-        "name": "Gemini",
+        "symbol": "♊",
+        "dateMin": "2000-05-22",
+        "dateMax": "2000-06-21",
         "element": "air",
         "stone": "agate"
     },
     "cancer": {
-        "name": "Cancer",
+        "symbol": "♋",
+        "dateMin": "2000-06-22",
+        "dateMax": "2000-07-22",
         "element": "water",
         "stone": "emerald"
     },
     "leo": {
-        "name": "Leo",
+        "symbol": "♌",
+        "dateMin": "2000-07-23",
+        "dateMax": "2000-08-22",
         "element": "fire",
         "stone": "onyx"
     },
     "virgo": {
-        "name": "Virgo",
+        "symbol": "♍",
+        "dateMin": "2000-08-23",
+        "dateMax": "2000-09-23",
         "element": "earth",
         "stone": "carnelian"
     },
     "libra": {
-        "name": "Libra",
+        "symbol": "♎",
+        "dateMin": "2000-09-24",
+        "dateMax": "2000-10-23",
         "element": "air",
         "stone": "chrysolite"
     },
     "scorpio": {
-        "name": "Scorpio",
+        "symbol": "♏",
+        "dateMin": "2000-10-24",
+        "dateMax": "2000-11-22",
         "element": "water",
         "stone": "beryl"
     },
     "sagittarius": {
-        "name": "Sagittarius",
+        "symbol": "♐",
+        "dateMin": "2000-11-23",
+        "dateMax": "2000-12-21",
         "element": "fire",
         "stone": "topaz"
     },
     "capricorn": {
-        "name": "Capricorn",
+        "symbol": "♑",
+        "dateMin": "2000-12-22",
+        "dateMax": "2000-01-20",
         "element": "earth",
         "stone": "ruby"
     },
     "aquarius": {
-        "name": "Aquarius",
+        "symbol": "♒",
+        "dateMin": "2000-01-21",
+        "dateMax": "2000-02-19",
         "element": "air",
         "stone": "garnet"
     },
     "pisces": {
-        "name": "Pisces",
+        "symbol": "♓",
+        "dateMin": "2000-02-20",
+        "dateMax": "2000-03-20",
         "element": "water",
         "stone": "amethyst"
     }
 }
 
-const signs = [{
-    number: 1,
-    name: "Aries",
-    symbol: "♈",
-    imageUrl: this.svgImagePath + "aries.svg",
-    element: "fire"
-}, {
-    number: 2,
-    name: "Taurus",
-    symbol: "♉",
-    imageUrl: this.svgImagePath + "taurus.svg",
-    element: "earth"
-}, {
-    number: 3,
-    name: "Gemini",
-    symbol: "♊",
-    imageUrl: this.svgImagePath + "gemini.svg",
-    element: "wind"
-}, {
-    number: 4,
-    name: "Cancer",
-    symbol: "♋",
-    imageUrl: this.svgImagePath + "cancer.svg",
-    element: "water"
-}, {
-    number: 5,
-    name: "Leo",
-    symbol: "♌",
-    imageUrl: this.svgImagePath + "leo.svg",
-    element: "fire"
-}, {
-    number: 6,
-    name: "Virgo",
-    symbol: "♍",
-    imageUrl: this.svgImagePath + "virgo.svg",
-    element: "earth"
-}, {
-    number: 7,
-    name: "Libra",
-    symbol: "♎",
-    imageUrl: this.svgImagePath + "libra.svg",
-    element: "wind"
-}, {
-    number: 8,
-    name: "Scorpio",
-    symbol: "♏",
-    imageUrl: this.svgImagePath + "scorpio.svg",
-    element: "water"
-}, {
-    number: 9,
-    name: "Sagittarius",
-    symbol: "♐",
-    imageUrl: this.svgImagePath + "sagittarius.svg",
-    element: "fire"
-}, {
-    number: 10,
-    name: "Capricorn",
-    symbol: "♑",
-    imageUrl: this.svgImagePath + "capricorn.svg",
-    element: "earth"
-}, {
-    number: 11,
-    name: "Aquarius",
-    symbol: "♒",
-    imageUrl: this.svgImagePath + "aquarius.svg",
-    element: "wind"
-}, {
-    number: 12,
-    name: "Pisces",
-    symbol: "♓",
-    imageUrl: this.svgImagePath + "pisces.svg",
-    element: "water"
-}];
+
+function loose(x){
+    const dur = document.querySelector('.durascop');
+    dur.innerHTML += `<span>${x}&nbsp;</span>`;
+}
+
+function looseClean(){
+    const dur = document.querySelector('.durascop').innerText;
+    document.querySelector('.durascop').innerHTML = '<span>' + dur.replace(/\s/g, '') + '</span>';
+}
+
+setTimeout('loose("DU")', 500);
+setTimeout('loose("RA")', 1700);
+setTimeout('loose("S")', 2200);
+setTimeout('loose("COP")', 2800);
+setTimeout('looseClean()', 3000);
+
+document.querySelector('.goAway').onclick = () => {
+    const dataChoose = document.querySelector('#dataChoose').value
+    const dCh = dataChoose.split('-');
+    let zodiac = '';
+    if (dCh[1] === '03'){
+        zodiac = +dCh[2] >= 21 ? "aries" : "pisces";
+    } else if (dCh[1] === '04'){
+        zodiac = +dCh[2] >= 21 ? "taurus" : "aries";
+    } else if (dCh[1] === '05'){
+        zodiac = +dCh[2] >= 22 ? "gemini" : "taurus";
+    } else if (dCh[1] === '06'){
+        zodiac = +dCh[2] >= 22 ? "cancer" : "gemini";
+    } else if (dCh[1] === '07'){
+        zodiac = +dCh[2] >= 23 ? "leo" : "cancer";
+    } else if (dCh[1] === '08'){
+        zodiac = +dCh[2] >= 23 ? "virgo" : "leo";
+    } else if (dCh[1] === '09'){
+        zodiac = +dCh[2] >= 24 ? "libra" : "virgo";
+    } else if (dCh[1] === '10'){
+        zodiac = +dCh[2] >= 24 ? "scorpio" : "libra";
+    } else if (dCh[1] === '11'){
+        zodiac = +dCh[2] >= 23 ? "sagittarius" : "scorpio";
+    } else if (dCh[1] === '12'){
+        zodiac = +dCh[2] >= 22 ? "capricorn" : "sagittarius";
+    } else if (dCh[1] === '01'){
+        zodiac = +dCh[2] >= 21 ? "aquarius" : "capricorn";
+    }else if (dCh[1] === '02'){
+        zodiac = +dCh[2] >= 20 ? "pisces" : "aquarius";
+    }else{
+        document.querySelector('.curr').innerText = 'ВАМ БЫЛО ВЕЛЕНО ВЫБРАТЬ ДАТУ\n ЧТО ЧТО В ЭТОМ НЕПОНЯТНОГО???'
+    }
+    document.querySelector('.curr').innerText = zodiac;
+}
