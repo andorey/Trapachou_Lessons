44f110e2dac714b07a3b82ed2c0c7b84a071d641
andoreyrich <andoreyrich@gmail.com>
2020-07-11 00:38:05 +0200

    with and without regular expression

--- game_called_cities/script.js
+++ game_called_cities/script.js
@@ -1,4 +1,15 @@
 //
 
-let field   = document.querySelector('#field');
+const arrCities = ['Atlanta', 'Alma-Ata', 'Alchevsk', 'Kharkov'];
+
+let field = document.querySelector('#field');
 let message = document.querySelector('#message');
+
+field.addEventListener('input', function () {
+    const letter = field.value.trim()
+
+    //const re = RegExp(`(?:^|\\s)([${letter[letter.length-1]}][a-z]*)`, 'gi');
+
+    const word = arrCities.filter(el => el.toLowerCase()[0] === letter.slice(letter.length -1))
+    message.innerHTML = word[0]
+})
